5b9af2d0baa0d7b68bb57919a50ed9e29b063549
hacker44-h44 <hacker.ht44@gmail.com>
2016-03-15 01:43:44 +0330

    Update Ingroup_enfa.lua

--- plugins/Ingroup_enfa.lua
+++ plugins/Ingroup_enfa.lua
@@ -1346,7 +1346,7 @@ return {
   "^[!/](setphoto)$",
   "^[!/](promote) (.*)$",
   "^[!/](promote)",
-  "^[!/](hef.lp)$",
+  "^[!/](help)$",
   "^[!/](clean) (.*)$",
   "^[!/](kill) (chat)$",
   "^[!/](kill) (realm)$",
