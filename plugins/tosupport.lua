6dbbee08e28fef68d84c1a2309ea4457d015d18a
hacker44 <hacker.ht44@gmail.com>
2016-03-16 17:32:13 +0330

    Added files via upload

---
+++ plugins/tosupport.lua
@@ -0,0 +1,19 @@
+--- https://github.com/amirhmz
+--- https://telegram.me/AmirDark
+do
+
+function run(msg, matches)
+  return " The Support Invition Link : \n https://telegram.me/joinchat/C9SSWgjLH0QR2vHxlcoTLQ
+  end
+return {
+  description = "shows support link", 
+  usage = "!tosupport : Return supports link",
+  patterns = {
+    "^([!/]tosupport)$",
+    "^(tosupport)$"
+  },
+  run = run
+}
+end
+--- https://github.com/amirhmz
+--- https://telegram.me/AmirDark
